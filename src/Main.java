8094d73eaa328aa9b618f379f51650e30a72cc06
Voronovich2021 <vovavoronovic72@gmail.com>
2023-11-14 16:24:52 +0300

    Generic

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         ThreeGen<Integer, String, Double> threeGenObj = new ThreeGen<Integer, String, Double>(77, "Обобщённый текст", 2.2);
         System.out.println(threeGenObj);
+
     }
 }
